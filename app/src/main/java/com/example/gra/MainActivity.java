dc6496033cbf8dc3824b1df389f62e9ef1e29333
4THP <gryba@zspswiecie,pl>
2022-10-20 11:03:01 +0200

    Commit Message

--- app/src/main/java/com/example/gra/MainActivity.java
+++ app/src/main/java/com/example/gra/MainActivity.java
@@ -1,132 +1,122 @@
 package com.example.gra;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity implements View.OnClickListener {
+    int gracz = 0;
     Button kolko;
     Button krzyzyk;
-    Button Button;
-    Button Button2;
-    Button Button3;
-    Button Button4;
-    Button Button5;
-    Button Button6;
-    Button Button7;
-    Button Button8;
-    Button Button9;
+    Button button;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        gra();
-
-
-    }
-
-    private void gra() {
         setContentView(R.layout.activity_main);
         kolko=findViewById(R.id.kolko);
         krzyzyk=findViewById(R.id.krzyzyk);
-        Button=findViewById(R.id.button);
-        Button2=findViewById(R.id.button2);
-        Button3=findViewById(R.id.button3);
-        Button4=findViewById(R.id.button4);
-        Button5=findViewById(R.id.button5);
-        Button6=findViewById(R.id.button6);
-        Button7=findViewById(R.id.button7);
-        Button8=findViewById(R.id.button8);
-        Button9=findViewById(R.id.button9);
-        kolko.setOnClickListener((View.OnClickListener) this);
-        krzyzyk.setOnClickListener((View.OnClickListener) this);
+        button=findViewById(R.id.button);
+        button2=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
+        button4=findViewById(R.id.button4);
+        button5=findViewById(R.id.button5);
+        button6=findViewById(R.id.button6);
+        button7=findViewById(R.id.button7);
+        button8=findViewById(R.id.button8);
+        button9=findViewById(R.id.button9);
+        kolko.setOnClickListener(this);
+        krzyzyk.setOnClickListener(this);
+        button.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+
+
     }
+
     public void onClick(View v){
-        int gracz=0;
-        switch(v.getId()){
-            case R.id.kolko:
+
+        if(v.getId()==R.id.kolko)
+            {
                 Toast.makeText(getApplicationContext(), "kółko", Toast.LENGTH_SHORT).show();
                 gracz = 1;
-                break;
-            case R.id.krzyzyk:
-                Toast.makeText(getApplicationContext(), "krzyżyk", Toast.LENGTH_SHORT).show();
-                gracz = 2;
-                break;
-        }
-        if(gracz==1)
-        {
-        switch(v.getId()){
-            case R.id.button:
-
-                break;
-            case R.id.button2:
-
-                break;
-            case R.id.button3:
-
-                break;
-            case R.id.button4:
-
-                break;
-            case R.id.button5:
-
-                break;
-            case R.id.button6:
-
-                break;
-            case R.id.button7:
-
-                break;
-            case R.id.button8:
-
-                break;
-            case R.id.button9:
-
-                break;
-
-        }
-    }
-        else if(gracz==2)
-        {
-            switch(v.getId()){
-                case R.id.button:
-
-                    break;
-                case R.id.button2:
-
-                    break;
-                case R.id.button3:
-
-                    break;
-                case R.id.button4:
-
-                    break;
-                case R.id.button5:
-
-                    break;
-                case R.id.button6:
-
-                    break;
-                case R.id.button7:
-
-                    break;
-                case R.id.button8:
-
-                    break;
-                case R.id.button9:
-
-                    break;
-
             }
+        if(v.getId()==R.id.krzyzyk)
+        {   Toast.makeText(getApplicationContext(), "krzyżyk", Toast.LENGTH_SHORT).show();
+                gracz = 2;
+
         }
-        else
-        {
-            Toast.makeText(getApplicationContext(), "wybierz czym grasz", Toast.LENGTH_SHORT).show();
+            if (v.getId() == R.id.button && gracz==2) {
+                button.setText("X");
+            }
+            else if (v.getId() == R.id.button2 && gracz==2) {
+                button2.setText("X");
+            }
+            else if (v.getId() == R.id.button3 && gracz==2) {
+                button3.setText("X");
+            }
+            else if (v.getId() == R.id.button4 && gracz==2) {
+                button4.setText("X");
+            }
+            else if (v.getId() == R.id.button5 && gracz==2) {
+                button5.setText("X");
+            }
+            else if (v.getId() == R.id.button6 && gracz==2) {
+                button6.setText("X");
+            }
+            else if (v.getId() == R.id.button7 && gracz==2) {
+                button7.setText("X");
+            }
+            else if (v.getId() == R.id.button8 && gracz==2) {
+                button8.setText("X");
+            }
+            else if (v.getId() == R.id.button9 && gracz==2) {
+                button9.setText("X");
+            }
+        if (v.getId() == R.id.button && gracz==1) {
+            button.setText("O");
         }
+        else if (v.getId() == R.id.button2 && gracz==1) {
+            button2.setText("O");
+        }
+        else if (v.getId() == R.id.button3 && gracz==1) {
+            button3.setText("O");
+        }
+        else if (v.getId() == R.id.button4 && gracz==1) {
+            button4.setText("O");
+        }
+        else if (v.getId() == R.id.button5 && gracz==1) {
+            button5.setText("O");
+        }
+        else if (v.getId() == R.id.button6 && gracz==1) {
+            button6.setText("O");
+        }
+        else if (v.getId() == R.id.button7 && gracz==1) {
+            button7.setText("O");
+        }
+        else if (v.getId() == R.id.button8 && gracz==1) {
+            button8.setText("O");
+        }
+        else if (v.getId() == R.id.button9 && gracz==1) {
+            button9.setText("O");
+        }
+
+
+    }
     }
 
-}
